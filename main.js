cfe0d85d94c715c6d01ccbe1ae00110cc115c2d5
Olaegbe Samuel <olaegbesamuel@gmail.com>
2018-01-31 03:14:15 -0800

    Added more comments to explain the code

--- main.js
+++ main.js
@@ -20,11 +20,25 @@ new TaskCollector([
 
 //EXAMPLE TWO
 
+/*		
+	The difference between "let, var and const"
+
+	var - With this sort of variable declaration, there is variable hoisting (only in JS).
+		  such that every variable declaration goes to the top of the page.
+	
+	let - This variable declaration only works within the block it was declared in. (e.g - within an if block)
+
+	const - This sets an apparent constant variable such that the variable cannot be re-assigned but the value can
+			be changed using some array or string functions like {variableName.push(newValue)}
+
+*/
+
 let names = ['Samuel', 'Emmanuel', 'Segun', 'Adams'];
 
 names = names.map(name => name + ' is great.');
 
 console.log(names);
+
 
 /*		3 - setting default values for functions in es6		*/
 
